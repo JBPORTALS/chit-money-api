f22b1f02e4d7d5f82bd31dad7a86ff58e00d8e83
JB PORTALS <118927681+JBPORTALS@users.noreply.github.com>
2025-04-17 11:34:52 +0530

    Add batches relationship to Organization model, implement realistic data generation in BatchFactory, and enhance CollectorSeeder to create nested relationships with Organizations and Batches. Remove unused seeders for BankDetail, Batch, Contact, and Organization.

--- database/factories/BatchFactory.php
+++ database/factories/BatchFactory.php
@@ -11,8 +11,16 @@ class BatchFactory extends Factory
 
     public function definition(): array
     {
-    	return [
-    	    //
-    	];
+        return [
+            "name" => $this->faker->company(),
+            "batch_type" => $this->faker->randomElement(["auction", "interest"]),
+            "starts_on" => $this->faker->date(),
+            "ends_on" => $this->faker->date(),
+            "due_date" => $this->faker->randomElement([20, 10, 15]),
+            "scheme" => $this->faker->randomElement([20, 10, 15]),
+            "fund_amount" => $this->faker->randomElement([200000, 500000, 100000]),
+            "batch_status" => $this->faker->randomElement(["upcoming", "active", "completed"]),
+            "commission_rate" => $this->faker->randomDigitNotZero()
+        ];
     }
 }
--- database/seeders/CollectorSeeder.php
+++ database/seeders/CollectorSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Batch;
 use App\Models\Collector;
+use App\Models\Organization;
 use Illuminate\Database\Seeder;
 
 class CollectorSeeder extends Seeder
@@ -12,6 +14,10 @@ class CollectorSeeder extends Seeder
      */
     public function run(): void
     {
-        Collector::factory()->count(5)->hasOrganizations(1)->create();
+        Collector::factory()->count(5)->has(
+            Organization::factory()->count(1)->has( //Every collector will have 1 Organization
+                Batch::factory()->count(3) // Every Organization will have 3 Batches in it
+            )
+        )->create();
     }
 }
